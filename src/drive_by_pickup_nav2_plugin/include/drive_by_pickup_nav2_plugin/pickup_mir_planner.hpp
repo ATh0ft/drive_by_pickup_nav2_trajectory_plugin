237ba5555dc5508e2e18e65a68ba8703e63f86c1
Alfred Thoft <alfchr21@student.aau.dk>
2024-03-08 13:23:39 +0100

    copy and build fixes

--- src/drive_by_pickup_nav2_plugin/include/drive_by_pickup_nav2_plugin/pickup_mir_planner.hpp
+++ src/drive_by_pickup_nav2_plugin/include/drive_by_pickup_nav2_plugin/pickup_mir_planner.hpp
@@ -1,5 +1,52 @@
-#ifndef NAV2_DRIVE_BY_PICKUP__DRIVE_BY_PLANNER_HPP_
-#define NAV2_DRIVE_BY_PICKUP__DRIVE_BY_PLANNER_HPP_
+/*
+ *-------------this code is modifide from the following-------------
+*/
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020 Shivang Patel
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Shivang Patel
+ *
+ * Reference tutorial:
+ * https://navigation.ros.org/tutorials/docs/writing_new_nav2planner_plugin.html
+ *********************************************************************/
+
+
+
+
+#ifndef DRIVE_BY_PICKUP_NAV2_PLUGIN__PICKUP_MIR_PLANNER_HPP_
+#define DRIVE_BY_PICKUP_NAV2_PLUGIN__PICKUP_MIR_PLANNER_HPP_
+
 
 #include <string>
 #include <memory>
@@ -17,7 +64,7 @@
 
 namespace drive_by_pickup_nav2_plugin{
 
-class DriveBy : public nav2_core::GobalPlanner{
+class DriveBy : public nav2_core::GlobalPlanner{
   public:
   DriveBy() = default;
   ~DriveBy() = default;
@@ -27,7 +74,7 @@ class DriveBy : public nav2_core::GobalPlanner{
     const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
     std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
     std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
-  ) override;
+
   // plugin cleanup
   void cleanup() override;
 
@@ -65,4 +112,5 @@ private:
 }
 
 
-#endif //NAV2_DRIVE_BY_PICKUP__DRIVE_BY_PLANNER_HPP_
+#endif //DRIVE_BY_PICKUP_NAV2_PLUGIN__PICKUP_MIR_PLANNER_HPP_
+
--- src/drive_by_pickup_nav2_plugin/src/pickup_mir_planner.cpp
+++ src/drive_by_pickup_nav2_plugin/src/pickup_mir_planner.cpp
@@ -1,10 +1,89 @@
+/*
+ *-------------this code is modifide from the following-------------
+*/
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020 Shivang Patel
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Shivang Patel
+ *
+ * Reference tutorial:
+ * https://navigation.ros.org/tutorials/docs/writing_new_nav2planner_plugin.html
+ *********************************************************************/
+
+
+
+
+
+
+#include "drive_by_pickup_nav2_plugin/pickup_mir_planner.hpp"
 #include <cstdio>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
 
-  printf("hello world drive_by_pickup_nav2_plugin package\n");
-  return 0;
+#include <cmath>
+#include <string>
+#include <memory>
+#include "nav2_util/node_utils.hpp"
+
+#include "drive_by_pickup_nav2_plugin/pickup_mir_planner.hpp"
+
+
+namespace drive_by_pickup_nav2_plugin{
+
+void DriveBy::configure(
+  const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+  std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros){
+  
+  node_ = parent.lock();
+  name_ = name;
+  tf_ = tf;
+  costmap_ = costmap_ros->getCostmap();
+  global_frame_ = costmap_ros->getGlobalFrameID();
+
+  // Parameter initialization
+  nav2_util::declare_parameter_if_not_declared(
+    node_, name_ + ".interpolation_resolution", rclcpp::ParameterValue(
+      0.1));
+  node_->get_parameter(name_ + ".interpolation_resolution", interpolation_resolution_);
+
+
+}
+
+  
+
+
+
+
+
 }
